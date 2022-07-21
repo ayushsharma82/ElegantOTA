5fd73bc273650d0a0613a7cd24bdde0ae7414d4a
Emil Muratov <gpm@hotplug.ru>
2022-07-21 15:10:11 +0530

    add example for esp32-flashz setup

---
+++ examples/demo_zlib/src/main.cpp
@@ -0,0 +1,50 @@
+ /*
+  ElegantOTA Demo Example - This example intended to work with ESP32 family chips
+  -----
+  Author: Ayush Sharma ( https://github.com/ayushsharma82 )
+  
+  Important Notice: Star the repository on Github if you like the library! :)
+  Repository Link: https://github.com/ayushsharma82/ElegantOTA
+*/
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+
+#include <ElegantOTA.h>
+
+const char* ssid = "WiFi_SSID";
+const char* password = "wifipass";
+
+WebServer server(80);
+
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "Hi! I am ESP32 :). You can upload some raw or zlib compressed images to me");
+  });
+
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+}
