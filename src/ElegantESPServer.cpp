e05b250e5bc955412ed07adecbaafa39e972fca6
Emil Muratov <gpm@hotplug.ru>
2022-07-21 13:27:11 +0530

    add esp32-flashz lib support for ESP32's bundled WebServer
    esp32 now can handle zlib compressed images (draft)

--- src/ElegantESPServer.cpp
+++ src/ElegantESPServer.cpp
@@ -1,6 +1,9 @@
 #if defined(ARDUINO) && (defined(ESP32) || defined(ESP8266)) && ELEGANTOTA_USE_ASYNC_SERVER == 0
 #include "ElegantESPServer.h"
 
+#if defined(ESP32)
+#include "flashz.hpp"
+#endif
 
 ElegantESPServer::ElegantESPServer() {
 }
@@ -47,11 +50,13 @@ ElegantESPServer::ElegantESPServer() {
         }
 
         // Get & Set MD5
+        #ifndef ESP32   // no way to process MD5 for esp32's compressed images
         if (_server->hasArg("MD5")) {
             if(!Update.setMD5(_server->arg("MD5").c_str())) {
                 return _server->send(400, "text/plain", "MD5 parameter invalid");
             }
         }
+        #endif
 
         // Handle File Upload
         HTTPUpload& upload = _server->upload();
@@ -79,21 +84,44 @@ ElegantESPServer::ElegantESPServer() {
                 size_t fsSize = ((size_t) &_FS_end - (size_t) &_FS_start);
                 uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
                 if (!Update.begin((cmd == U_FS)?fsSize:maxSketchSpace, cmd)){ // Start with max available size
-            #elif defined(ESP32)
-                int cmd = (upload.filename == "filesystem") ? U_SPIFFS : U_FLASH;
-                if (!Update.begin(UPDATE_SIZE_UNKNOWN, cmd)) { // Start with max available size
-            #endif
                     #if ELEGANTOTA_DEBUG == 1
-                        Update.printError(ELEGANTOTA_DEBUG_PORT);
+                        #ifdef ESP32
+                            FlashZ::getInstance().printError(ELEGANTOTA_DEBUG_PORT);
+                        #else
+                            Update.printError(ELEGANTOTA_DEBUG_PORT);
+                        #endif
                     #endif
                     _trigger_ota_fail();
                     return _server->send(500, "text/plain", "OTA update failed due to size constraints");
                 }
                 ELEGANTOTA_DEBUG_LOG("[ElegantOTA] OTA update Started: %s - %d bytes\n", upload.filename.c_str(), upload.totalSize);
+            #endif
+
         } else if (upload.status == UPLOAD_FILE_WRITE) {
             // Handle Upload Write
+            #if defined(ESP32)
+            if (!upload.totalSize){
+                int cmd = (upload.filename == "filesystem") ? U_SPIFFS : U_FLASH;
+                bool mode_z = (upload.buf[0] == ZLIB_HEADER);    // check if we have a compressed image
+
+                if (!(mode_z ? FlashZ::getInstance().beginz(UPDATE_SIZE_UNKNOWN, cmd) : FlashZ::getInstance().begin(UPDATE_SIZE_UNKNOWN, cmd))){
+                    #if ELEGANTOTA_DEBUG == 1
+                            FlashZ::getInstance().printError(ELEGANTOTA_DEBUG_PORT);
+                    #endif
+                    _trigger_ota_fail();
+                    return _server->send(500, "text/plain", "OTA update failed due to size constraints");
+                }
+                ELEGANTOTA_DEBUG_LOG("[ElegantOTA] OTA update Started: %s; mode_z: %s\n", upload.filename.c_str(), mode_z ? "yes" : "no");
+            }
+            #endif  // defined(ESP32)
+
+        #ifdef ESP32
+            if(FlashZ::getInstance().writez(upload.buf, upload.currentSize, false) != upload.currentSize){
+                FlashZ::getInstance().printError(ELEGANTOTA_DEBUG_PORT);
+        #else
             if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
                 Update.printError(ELEGANTOTA_DEBUG_PORT);
+        #endif
                 _trigger_ota_fail();
                 return _server->send(500, "text/plain", "OTA update failed due to error while writing to flash");
             }
@@ -102,7 +130,16 @@ ElegantESPServer::ElegantESPServer() {
             _trigger_ota_progress(progress);
         } else if (upload.status == UPLOAD_FILE_END) {
             // Handle Upload End
+        #ifdef ESP32
+            if(FlashZ::getInstance().writez(upload.buf, upload.currentSize, true) != upload.currentSize){
+                _trigger_ota_fail();
+                ELEGANTOTA_DEBUG_LOG("[ElegantOTA] OTA Update failed unexpectedly (likely broken connection): status=%d\n", upload.status);
+                return FlashZ::getInstance().abortz();
+            }
+            if(FlashZ::getInstance().endz()){
+        #else
             if (Update.end(true)) { //true to set the size to the current progress
+        #endif
                 ELEGANTOTA_DEBUG_LOG("[ElegantOTA] OTA update Success: %u\n", upload.totalSize);
                 _trigger_ota_success();
             } else {
@@ -118,6 +155,10 @@ ElegantESPServer::ElegantESPServer() {
 
             _trigger_ota_end();
         } else {
+            #ifdef ESP32
+                FlashZ::getInstance().abortz();
+            #endif
+
             ELEGANTOTA_DEBUG_LOG("[ElegantOTA] OTA Update failed unexpectedly (likely broken connection): status=%d\n", upload.status);
         }
     });
